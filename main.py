749d9e0fbb7455c3f29f1aacd0763d3a000ce343
GorasaAkshara <gorasaakshara@gmail.com>
2025-12-18 00:37:03 +0530

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 import difflib
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,65 +14,50 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-songs_data = pd.read_csv("songs.csv")
 
-if "index" not in songs_data.columns:
-    songs_data.reset_index(inplace=True)
+movies_data = pd.read_csv("movies.csv")
 
-selected_features = ["artist", "song", "text"]
+selected_features = ["genres", "keywords", "tagline", "cast", "director"]
 
 for feature in selected_features:
-    songs_data[feature] = songs_data[feature].fillna("")
+  movies_data[feature] = movies_data[feature].fillna("")
 
-combined_features = (
-    songs_data["artist"] + " " +
-    songs_data["song"] + " " +
-    songs_data["text"]
-)
+combined_features = movies_data['genres']+' '+ movies_data['keywords']+' '+ movies_data['tagline']+' '+ movies_data['cast']+' '+ movies_data['director']
 
 vectorizer = TfidfVectorizer()
 feature_vectors = vectorizer.fit_transform(combined_features)
 
-
 similarity = cosine_similarity(feature_vectors)
 
-list_of_all_titles = songs_data["title"].tolist()
+list_of_all_titles = movies_data['title'].to_list()
 
-
-class SongRequest(BaseModel):
-    song: str
+class MovieRequest(BaseModel):
+    movie: str
 
 @app.get("/")
 def root():
-    return {"message": "Song Recommendation API"}
+   return {"message": "Movie Recommendation API"}
 
 @app.post("/recommendation")
-def recommend(request: SongRequest):
-    song_name = request.song
+def recommend(request: MovieRequest):
+    movie_name = request.movie
+    find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+    close_match = find_close_match[0]
 
-    close_matches = difflib.get_close_matches(song_name, list_of_all_titles)
-    
-    if not close_matches:
-        return {"error": "Song not found"}
+    index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
 
-    close_match = close_matches[0]
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
 
-    index_of_song = songs_data[songs_data.title == close_match]["index"].values[0]
-
-    similarity_scores = list(enumerate(similarity[index_of_song]))
-
-    sorted_similar_songs = sorted(
-        similarity_scores, key=lambda x: x[1], reverse=True
-    )
+    sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
 
     recommendations = []
 
-    for song in sorted_similar_songs[1:21]:
-        index = song[0]
-        title = songs_data[songs_data.index == index]["song"].values[0]
-        recommendations.append(title)
+    for i, movie in enumerate(sorted_similar_movies[1:30]):
+       index= movie[0]
+       title = movies_data[movies_data.index == index]['title'].values[0]
+       recommendations.append(title)
 
     return {
-        "matched_song": close_match,
-        "recommendations": recommendations
+       "matched_movies": close_match,
+       "recommendations": recommendations
     }
